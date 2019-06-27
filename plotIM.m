1786f1902b74c17b199abd72f16c3ca308285fec
Elliot Padgett <elliot.padgett@gmail.com>
2019-06-26 23:08:47 -0400

    Function for nice image plots

---
+++ plotIM.m
@@ -0,0 +1,20 @@
+function varargout=plotIM(varargin)
+% Specialized version of imagesc using nice-looking defaults (grayscale, no
+% ticks if scale undefined, proper aspect ratio, big font) for  electron 
+% microscopy images. Takes an argument string in the same form as imagesc.
+
+im = imagesc(varargin{:});
+
+if ~isvector(varargin{1})
+    %if x and y scales are not specified, don't show axis ticks
+    set(gca,'XTick',[],'YTick',[]);
+end
+set(gca,'FontSize',22,'FontWeight','Bold');
+axis image;
+colormap(gray);
+
+if nargout
+    varargout{1} = im;
+end
+
+end
